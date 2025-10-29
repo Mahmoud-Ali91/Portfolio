4f36d777687c58924e4cffb274d5a3a32f3a415d
Mahmoud Ali <drmahmoodali91@gmail.com>
2025-10-30 01:42:12 +0300

    Updated portfolio structure with prototype pages and assets

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+const Home = () => {import React from 'react';
+
+  return (
+
+    <div className="home">const Home = () => {
+
+      <h1>Welcome to My Portfolio</h1>  return (
+
+      <p>Hello! I'm a professional who is passionate about delivering high-quality work.</p>    <div className="home">
+
+    </div>      <h1>Welcome to My Portfolio</h1>
+
+  );      {/* Add your home page content here */}
+
+};    </div>
+
+  );
+
+export default Home;};
+
+export default Home;
